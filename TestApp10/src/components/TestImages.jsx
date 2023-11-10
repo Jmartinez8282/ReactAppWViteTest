34860b1239f0e8e2244d9d9e8c4322f950d873df
Jose Luis Martinez Lopez <josemartinez8282@gmail.com>
2023-11-09 18:40:28 -0800

    first commit

---
+++ TestApp10/src/components/TestImages.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import soccerfield from '../assets/Images/soccerfield.jpg'
+
+const TestImages = () => {
+  return (
+    <div>
+      <img style={{width: 500}} src={soccerfield}alt="" />
+    </div>
+  )
+}
+
+export default TestImages
